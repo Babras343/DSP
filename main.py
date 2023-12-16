3944d2be7b831b68b363dcc460a908fd56a13d82
BARRALES369 <utp0006760@alumno.utpuebla.edu.mx>
2023-12-16 15:49:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,20 @@
-#from AI import *
-#from DataProcessing import *
-#from GUI import *
-from InfluxDB_RD import *
-#from InfluxDC_PD import *
+# main.py
+from AI import run_AI
+from DataProcessing import *
+from GUI import *
 from SSS import *
-import subprocess
+
+import asyncio
 
 async def main():
-    SSS_task = asyncio.create_task(activate_SSS())
+    
+    # Create tasks for each module
+    task_SSS = asyncio.create_task(run_SSS())
+    task_AI = asyncio.create_task(run_AI())
+    task_Data_Processing = asyncio.create_task(run_Data_Processing())
 
-    # Create InfluxDB JSON object:
-    influx_data = [
-        {
-        "measurement": "measurement_name",
-        "time": "timestamp",
-        "fields": {
-            "SensorName": ("SensorName", ""),
-            "x": float(("x", 0.0)),
-            "y": float(("y", 0.0)),
-            "z": float(("z", 0.0)),
-                    }
-        }
-        ]
+    # Wait for all tasks to complete
+    await asyncio.gather(task_SSS, task_AI, task_Data_Processing)
 
-    data = influx_data
-
-    send_to_influxdb(data)
-
-    await asyncio.Future()
+# Run the event loop
+asyncio.run(main())
